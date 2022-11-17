e21aeab74d8fa0e38b8dd692e5980b0cdef336b0
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-11-17 22:31:38 +0530

    Add project files.

---
+++ practice2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace practice2
+{
+  public  class program
+    {
+        public static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+            Anagram.boolean();
+           // tempConversion temp = new tempConversion();
+            //tempConversion.Conversion();
+        }
+    }
+}
---
+++ practice2/tempConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    public class tempConversion
+    {
+
+        public static void Conversion()
+        {
+            double fahrenheit;
+            double celcius = 36;
+            Console.WriteLine("Celcius:" + celcius);
+            fahrenheit = (celcius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit:" + fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
